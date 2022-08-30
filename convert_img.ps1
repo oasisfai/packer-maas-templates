c153489ebb83450e2421d02f2e3d027c2c38eb38
oasisfai <83398060+oasisfai@users.noreply.github.com>
2022-08-30 15:50:04 +0800

    Update convert_img.ps1

--- convert_img.ps1
+++ convert_img.ps1
@@ -1,8 +1,8 @@
 Write-Host 'Converting artifact to raw format'
 
 $current_path = [string](Get-Location)
-$input_artifact = $current_path + '.\output-hyperv-iso\Virtual Hard Disks\ubuntu-desktop.vhdx'
-$output_img = $current_path + '.\output-hyperv-iso\Virtual Hard Disks\packer-qemu'
+$input_artifact = $current_path + '\output-hyperv-iso\Virtual Hard Disks\ubuntu-desktop.vhdx'
+$output_img = $current_path + '\output-hyperv-iso\Virtual Hard Disks\packer-qemu'
 
 if ( Test-Path -Path $input_artifact -PathType leaf) {
 	qemu-img convert -f vhdx -O raw $input_artifact  $output_img -p
